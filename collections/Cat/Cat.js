74a4c511d72f40613cc048fe88cc3de81ce4f7dc
Cristian E. Perrone <cristianperrone.web@gmail.com>
2016-10-16 20:12:40 +0000

    Add Cat Collection

---
+++ collections/Cat/Cat.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/test');
+
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'Zildjian' });
+kitty.save(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('meow');
+  }
+});
--- server.js
+++ server.js
@@ -4,20 +4,7 @@ var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
 var fs = require('fs');
 /// Mongoose
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/test');
-
-var Cat = mongoose.model('Cat', { name: String });
-
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('meow');
-  }
-});
+require('collections/Cat/Cat');
 /// /Mongose
 
 var app = express();
